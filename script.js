62f2fea282e81271e07b2598ac07ffbab684155c
contado-liyah <liyahleighcontado@gmail.com>
2026-03-08 20:15:29 +0800

    Add navbar toggle functionality

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+navbarToggle.addEventListener('click', () => {
+    navbarToggle.classList.toggle('active');
+    navbarMenu.classList.toggle('active');
+});
